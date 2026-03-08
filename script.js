3293533086a2a1722972ce352fd78fba83cb9415
plantloser <lvillafanegarcia@mail.valenciacollege.edu>
2026-03-08 17:24:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function videoFunction() {
+            document.getElementById("myComment").innerHTML += "A Awsome Puma Video!";
+        }
+
+function emailMessage() {
+    document.getElementById("emailBox").innerHTML += "Please enter a vailid email address!";
+}
+
+function blankMessage() {
+    document.getElementById("emailBox").innerHTML = "Email:";
+} 
